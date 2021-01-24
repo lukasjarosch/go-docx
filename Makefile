a3198e9ed2c3b78bb34a6c70a1c072f43887c8f0
Lukas Jarosch <lukas.jarosch@mail.com>
2021-01-24 23:29:51 +0100

    add simple Replace test to verify that the replaced docx is actually valid xml

--- Makefile
+++ Makefile
@@ -1,9 +1,4 @@
-GOLANG_CI_LINT_VERSION="v1.31.0"
-
-lint:
-	@docker run --rm -v $(pwd):/app -w /app golangci/golangci-lint:${GOLANG_CI_LINT_VERSION} golangci-lint run -v
-
-
 .PHONY: test
 test:
 	@go test -v .
+
---
+++ replace_test.go
@@ -0,0 +1,57 @@
+package docx
+
+import (
+	"encoding/xml"
+	"os"
+	"testing"
+)
+
+func TestReplacer_Replace(t *testing.T) {
+	replaceMap := PlaceholderMap{
+		"key":                         "key",
+		"key-with-dash":               "key-with-dash",
+		"key-with-dashes":             "key-with-dashes",
+		"key with space":              "key with space",
+		"key_with_underscore":         "key_with_underscore",
+		"multiline":                   "multiline",
+		"key.with.dots":               "key.with.dots",
+		"mixed-key.separator_styles#": "mixed-key.separator_styles#",
+		"yet-another_placeholder":     "yet-another_placeholder",
+		"foo":                         "foo",
+	}
+
+	doc, err := Open("./test/template.docx")
+	if err != nil {
+		t.Error("unable to open testfile", err)
+		return
+	}
+
+	err = doc.ReplaceAll(replaceMap)
+	if err != nil {
+		t.Error("replacing failed", err)
+		return
+	}
+
+	err = doc.WriteToFile("./test/out.docx")
+	if err != nil {
+		t.Error("unable to write", err)
+		return
+	}
+
+	document, err := Open("./test/out.docx")
+	if err != nil {
+		t.Error("failed to open docx")
+		return
+	}
+
+	documentXml := document.files[DocumentXml]
+
+	err = xml.Unmarshal(documentXml, new(interface{}))
+	if err != nil {
+		t.Error("failed to unmarshal xml, replacing failed")
+		return
+	}
+
+	// cleanup
+	_ = os.Remove("./test/out.docx")
+}
